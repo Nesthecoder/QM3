cbb58ca338004d6e5ee00eb83f6acf39509f6eb7
Nesthecoder <neslihan.guven@barcelonagse.eu>
2021-05-04 16:48:34 +0200

    ar2filter

---
+++ myar2filter.m
@@ -0,0 +1,15 @@
+function [mu,eps] = myar2filter(y,param)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+mu = zeros(length(y),1);
+eps = zeros(length(y),1);
+
+mu(1) = c/(1-param(1)-param(2));
+eps(1) = y(1) - mu(1);
+
+for t = 3:(length(y)+1)
+    
+    mu(t) = param(1) + param(2)*y(t-1) + param(3)*y(t-2);
+    eps(t) = y(t) - mu(t);
+end
+
